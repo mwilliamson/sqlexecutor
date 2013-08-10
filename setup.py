1059443bc5a558a3fbc9fa2ece3a8fadeaf054a5
Michael Williamson <mike@zwobble.org>
2013-08-10 22:13:49 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='sqlexecutor',
-    version='0.1.0',
+    version='0.1.1',
     description='Execute SQL queries in an isolated database',
     long_description=read("README"),
     author='Michael Williamson',
